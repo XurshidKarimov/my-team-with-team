02599ff7ff3e234c6c6ff06684b32d8ef9c9ec13
Xurshid <kaxudi34801@gmail.com>
2022-12-08 04:31:20 +0500

    hamburger anime done

--- js/index.js
+++ js/index.js
@@ -2,14 +2,22 @@
 
 let h_icon = document.getElementById("hamburger-icon"),
   h_close = document.getElementById("close-icon"),
-  h_menu = document.getElementById("hamburger-menu");
+  h_menu = document.getElementById("hamburger-menu"),
+  shadow = document.getElementById("shadow"),
+  body = document.getElementsByTagName("body")[0];
 
 h_icon.addEventListener("click", () => {
+  shadow.classList.add("active");
   h_menu.classList.remove("close");
   h_menu.classList.add("open");
+
+  body.classList.add("opac");
 });
 
 h_close.addEventListener("click", () => {
+  shadow.classList.remove("active");
   h_menu.classList.remove("open");
   h_menu.classList.add("close");
+
+  body.classList.remove("opac");
 });
