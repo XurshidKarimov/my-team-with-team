2e167ca5246b46172a290ef6823e0db9b1e8b820
Xurshid <kaxudi34801@gmail.com>
2022-12-08 04:08:25 +0500

    hamburger menu done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-'use strict';
+"use strict";
+
+let h_icon = document.getElementById("hamburger-icon"),
+  h_close = document.getElementById("close-icon"),
+  h_menu = document.getElementById("hamburger-menu");
+
+h_icon.addEventListener("click", () => {
+  h_menu.classList.remove("close");
+  h_menu.classList.add("open");
+});
+
+h_close.addEventListener("click", () => {
+  h_menu.classList.remove("open");
+  h_menu.classList.add("close");
+});
